7218f3c05ca068b8563ef123954b737bd6fc1fb2
JJennings564 <jjennings564@gmail.com>
2024-09-05 14:45:45 -0500

    Fixed some errors and completed some methods

--- Project1.cpp
+++ Project1.cpp
@@ -17,7 +17,7 @@ int main () {
     cout << "First one in matrix format" << endl;
     (*firstOne).displayMatrix();
 
-    cout << "First one in sparse matrix format" << endl;
+   // cout << "First one in sparse matrix format" << endl;
     cout << (*firstOne);
 
     cout << "Second one in matrix format" << endl;
@@ -36,7 +36,7 @@ int main () {
     (*temp).displayMatrix();
 
     cout << "Matrix Multiplication Result" << endl;
-    
+
     temp = (*(*firstOne).Multiply(secondOne));
     cout << temp;
     (*temp).displayMatrix();
@@ -44,14 +44,34 @@ int main () {
 
 class SparseRow {
     protected:
-        int row; //Row#
-        int col; //Column#
-        int value; //We will assume that all our values will be integers
-    public:
-        SparseRow (); //default constructor; row=-1;col=-1;value=0
-        display(); // print Row#, Column#, value
-        ostream& operator<< (const SparseRow);
-        //other methods that are necessary such as get and set
+    int row;   // Row#
+    int col;   // Column#
+    int value; // We will assume that all our values will be integers
+public:
+    SparseRow(int r = -1, int c = -1, int v = 0); // default constructor; row=-1;col=-1;value=0
+    void display();                               // print Row#, Column#, value
+    friend ostream &operator<<(ostream& os, const SparseRow& sr) {
+        os << "Row: " << sr.row << ", Col: " << sr.col << ", Value: " << sr.value;
+        return os;
+    }
+
+    void display() {
+    cout << "Row: " << row << ", Col: " << col << ", Value: " << value << endl;
+    }
+    SparseRow(int r, int c, int v) {
+    row = r;
+    col = c;
+    value = v;
+    }
+
+    // other methods that are necessary such as get and set
+    int getRow() { return row; }
+    int getCol() { return col; }
+    int getValue() { return value; }
+
+    void setRow(int r) { row = r; }
+    void setCol(int c) { col = c; }
+    void setValue(int v) { value = v; }
 };
 
 class SparseMatrix {
@@ -63,7 +83,12 @@ class SparseMatrix {
         SparseRow* myMatrix; //Array
     public:
         SparseMatrix ();
-        SparseMatrix (int n, int m, int cv, int noNSV);
+        SparseMatrix (int n, int m, int cv, int noNSV){
+            noRows = n;
+            noCols = m;
+            commonValue = cv;
+            noNonSparseValues = noNSV;
+        }
         SparseMatrix* Transpose (){
         for(int i = 0; i < noCols; i++){
             for(int j = 0; i < noRows; i++){
@@ -73,7 +98,11 @@ class SparseMatrix {
         } //Matrix Transpose
         SparseMatrix* Multiply (SparseMatrix& M);
         SparseMatrix* Addition (SparseMatrix& M);
-        ostream& operator<< (const SparseMatrix& sm);
-        displayMatrix (); //Display the matrix in its original format
+        friend ostream &operator<<(ostream& os, const SparseRow);
+        void displayMatrix (){
+
+
+
+        } //Display the matrix in its original format
         //other methods that are necessary such as get and set
 };
